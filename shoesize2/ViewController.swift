a9756cdd40631978c8b6565ff01a64592515fa6a
John Mccolly <Johns@JohnMcCollysMacBook-3.local>
2014-12-24 06:06:36 -0600

    Initial Commit

---
+++ shoesize2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shoesize2
+//
+//  Created by John Mccolly on 12/24/14.
+//  Copyright (c) 2014 John Mccolly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
